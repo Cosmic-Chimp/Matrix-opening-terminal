6293e80c1f5c977211fc7b4c77affcdcf40fa394
Cosmic-Chimp <rubzdebruyn@gmail.com>
2022-04-27 13:29:41 +0200

    small-changes

--- index.php
+++ index.php
@@ -1,33 +1,21 @@
 <?php
-
-// > start program with "wake up neo", delay each word with built in functionality,
-//  but write custom func to make it a bit cleaner
-
-// > give usr ability to answer, but then the machine should respond with
-// "the matrix has got you" regardless of what the user answered/entered with
-
-// > add other cool things like after the "got you" part, let the user input something, but
-//  add conditionals that only responds on the input if the users asks a question
-
-
-
-// First lines:
-
-// > Wake up, neo...
-// > The Matrix has you...
-// > Follow the white rabbit.
-
 function Matrix()
 {
   echo "\nWake up neo...\n";
-  readline("you: ");
-
+  readline("> ");
 
   echo "\nThe Matrix has you...\n";
-  readline("you: ");
-
+  readline("> ");
 
   echo "\nFollow the white rabbit.\n";
-  // readline("you: ");
 }
 Matrix();
+
+
+// > start program with "wake up neo", delay each word in the future
+
+// > give user ability to answer, but then the machine should respond with
+// "the matrix has got you" regardless of what the user answered with
+
+// > add some conditionality to how the machine reacts later
+// > delay the way the text is displayed on the terminal, sort of like the scene in the movie, similar to a typerwriting animation
